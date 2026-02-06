2b63c7a3b17c41c18e818d104c7772c646ab6757
Denilson Bonatti <denilsonbonatti@gmail.com>
2026-02-06 15:17:26 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import customtkinter as ctk
 from tkinter import messagebox
+from PIL import Image
+import os
 
 
 # 1. Configurações Iniciais
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
+
+# Definir o caminho da imagem
+caminho_logo = os.path.join(os.path.dirname(__file__), "logo_shibakita.png")
+
+
+
+# Criar o objeto da imagem
+imagem_logo = ctk.CTkImage(
+    light_image=Image.open(caminho_logo),
+    dark_image=Image.open(caminho_logo),
+    size=(120, 120) # Ajuste o tamanho conforme necessário
+)
+
 
 # 2. Instanciação da Janela
 janela = ctk.CTk()
@@ -17,6 +32,10 @@ frame = ctk.CTkFrame(master=janela, corner_radius=15)
 frame.pack(pady=40, padx=40, fill="both", expand=True)
 
 # 4. Widgets
+
+# Inserir o logotipo no topo do frame
+label_imagem = ctk.CTkLabel(master=frame, image=imagem_logo, text="") # text="" remove o texto padrão
+label_imagem.pack(pady=(20, 10)) # Espaçamento superior e inferior
 
 label_titulo = ctk.CTkLabel(master=frame, text="Shibakita Eletro", font=("Roboto", 24, "bold"))
 label_titulo.pack(pady=20)
