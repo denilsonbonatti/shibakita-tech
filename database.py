7213aac944b9d9412dc309012f6987ebabcab100
Denilson Bonatti <denilsonbonatti@gmail.com>
2026-02-10 15:34:34 -0300

    Update database connection credentials and logic

--- database.py
+++ database.py
@@ -2,12 +2,12 @@ import pyodbc
 from tkinter import messagebox
 
 # Configurações do Servidor e Banco
-SERVIDOR = 'aulasenacjab.database.windows.net'
+SERVIDOR = 'xxxx'
 BANCO = 'ShibakitaEletro'
 
 # Credenciais do Usuário (definidas no portal Azure)
-USUARIO_APP = 'azure-senac' 
-SENHA_APP = 'MinhaSenha123##'
+USUARIO_APP = 'xxxx' 
+SENHA_APP = 'xxxx'
 
 def conectar():
     string_conexao = (
@@ -48,4 +48,5 @@ def realizar_login(login_digitado, senha_digitada):
         
         if resultado:
             return resultado[0] # Retorna 'Administrador' ou 'Operador'
+
     return None
